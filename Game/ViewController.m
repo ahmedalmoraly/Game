36b757f4ad2d1a5c9d022d3866b13186ce4367b7
Ahmad al-Moraly <ahmedalmoraly@gmail.com>
2012-12-13 00:29:12 +0200

    ..

--- Game/ViewController.m
+++ Game/ViewController.m
@@ -7,9 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "MessagesCell.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet MessagesCell *messagesCell;
 
+@property (strong, nonatomic) NSArray *messagesArray;
 @end
 
 @implementation ViewController
@@ -18,12 +21,73 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    // hide navigationbar
+    self.navigationController.navigationBarHidden = YES;
+    
+    [self loadMessages];
+}
+
+-(void)loadMessages
+{
+    NSDictionary *message = @{@"title" : @"Message title", @"sender" : @"Ahmad al-Moraly", @"date" : @"12/12/2012", @"body" : @"Message body message body message body message body message body message body message body", @"count" : @"3"};
+    
+    self.messagesArray = @[message, message, message, message, message, message, message, message, message, message, message,  message, message, message, message, message, message, message, message, message, message, message, message, message,  message, message];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.messagesArray.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"MessagesCell";
+    MessagesCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    
+    if (!cell)
+    {
+        [[NSBundle mainBundle] loadNibNamed:@"MessagesCell" owner:self options:nil];
+        cell = self.messagesCell;
+    
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    }
+    
+    if (indexPath.row % 2)
+    {
+        [cell setMessageBackgroundHidden:NO];
+        [cell setMessageRead:YES];
+    }
+    else
+    {
+        [cell setMessageBackgroundHidden:YES];
+        [cell setMessageRead:NO];
+    }
+    NSDictionary *message = self.messagesArray[indexPath.row];
+    
+    [cell setMessageSender:message[@"sender"]];
+    [cell setMessageTitle:message[@"title"]];
+    [cell setMessageDate:message[@"date"]];
+    [cell setMessageBody:message[@"body"]];
+    return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    ViewController *controller = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
+    
+    [self.navigationController pushViewController:controller animated:YES];
 }
 
 @end
